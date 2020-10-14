41a86c73acd6da4e3dd2f651b127a0c5a8adf3e6
koosbotha <kbotha80@hotmail.com>
2020-10-15 08:31:59 +1300

    Update Update_VM.ps1

--- Update_VM.ps1
+++ Update_VM.ps1
@@ -1,23 +1,24 @@
 ﻿Param($ResourceGroupName, $VMName)
 #Start Logging
-Write-Output "Starting Powershell update $(Get-date)" 
+    Write-Output "Starting Powershell update $((get-date).AddHours(12))" 
+
 $vm = Get-AzVM -ResourceGroupName $ResourceGroupName -Name $VMName
 $vm.LicenseType = 'Windows_Server'
+
 Write-Output "Target VM: $($Vm.Name)"
+
 $tags = (Get-AzResource -ResourceId $vm.id).Tags
-if (-not($tags.ContainsKey('Created by')))
-{
-Write-Output "Searching Activity log for Creator..." 
-$tags.'Created by'=(((Get-AzActivityLog -ResourceId $vm.Id |`
- ?{$_.OperationName.Value -eq 'Microsoft.Compute/virtualMachines/write' `
- -and $_.SubStatus.value -eq 'Created'} ) | select -first 1).Caller -split '@' )[0]
-$tags.'Remediated'=(Get-Date)
+    if (-not($tags.ContainsKey('Created by')))
+       {Write-Output "Searching Activity log for Creator..." 
+        
+            $tags.'Created by'=(((Get-AzActivityLog -ResourceId $vm.Id |where-object{$_.OperationName.Value -eq 'Microsoft.Compute/virtualMachines/write' -and $_.SubStatus.value -eq 'Created'} ) | select -first 1).Caller -split '@' )[0]
+            $tags.'Remediated'=((get-date).AddHours(12))
 }
+#Update resources
 Update-AzVM -VM $vm -ResourceGroupName $ResourceGroupName
 Update-AzTag -ResourceId $vm.id -Tag $tags -Operation Merge
-Write-Output "...Deployment Script complete," 
 
-
+Write-Output "...Deployment Script complete...$((get-date).AddHours(12))" 
 
 
 
